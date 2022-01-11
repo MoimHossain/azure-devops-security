3c85a7c78dca48918343cf528f84d70a49132575
Moim HA <moimhossain@gmail.com>
2022-01-11 07:21:26 +0100

    Added an example project

---
+++ src/example-usage/Tribe-Collection/process.sh
@@ -0,0 +1,20 @@
+PAT=$1
+commitSha=$BUILD_SOURCEVERSION
+
+echo "Detecting changes...$commitSha"
+changedFiles=$(git diff-tree --no-commit-id --name-only -r $commitSha) 
+workloads=()
+for d in Tribes/*/ ; do  
+    if [[ $changedFiles == *"$d"* ]]; then
+    workloads+=($d)
+    fi
+done
+
+for wl in ${workloads[@]}; do
+    workloadName="$(basename $wl)"
+    echo "<<$workloadName>> changed (full path: $wl)"
+    directorypath="${BUILD_SOURCESDIRECTORY}/Tribes/${workloadName}"
+    echo "source directory $directorypath"
+
+    docker run --rm -e AzDOAADJoinedPAT="$PAT" -e AzDOAADJoinedURL="https://dev.azure.com/moim/" -v "$directorypath:/home/payload" docker.io/moimhossain/adoctl:v2 -d "/home/payload"
+done
